f773d307a07bb958228d8e00c748b16d498cfd5f
04163917292 <04163917292@JIPA-LABIN03-12.ifro.local>
2018-05-11 17:28:41 -0400

    intalação do jfroenix

--- 3b_emanuel/src/br/edu/ifro/menupricipalController.java
+++ 3b_emanuel/src/br/edu/ifro/menupricipalController.java
@@ -11,6 +11,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 
 /**
  *
@@ -19,12 +21,18 @@ import javafx.scene.control.Label;
 public class menupricipalController implements Initializable {
     
     @FXML
-    private Label label;
+    private TextField txtNumero1,txtNumero2, txtResultado;
     
+     @FXML
+    
+     private Button bntSoma;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void soma (ActionEvent event) {
+        double num1 = Double.parseDouble(txtNumero1.getText());
+        double num2 = Double.parseDouble(txtNumero2.getText());
+        double resultado = num1 +num2 ;
+        
+        txtResultado.setText(resultado.toString());
     }
     
     @Override
